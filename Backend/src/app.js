f813bae7e9f9559d567b5e47721c01e73c019609
pratiksha-shinde01 <sumanshinde463@gmail.com>
2026-04-11 18:32:38 +0530

    Add Login and Register pages with authentication functionality

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,11 +6,6 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    methods: [ "GET", "POST", "PUT", "DELETE" ],
-    credentials: true
-}))
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
