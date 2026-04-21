62bc36914d215d1f7e075ac33b8b285e760281a4
pratiksha-shinde01 <sumanshinde463@gmail.com>
2026-04-21 14:03:14 +0530

    implement and  product varient functionality  with image upload support

--- Frontend/src/features/products/services/product.api.js
+++ Frontend/src/features/products/services/product.api.js
@@ -25,3 +25,19 @@ export async function getProductById(productId) {
     const response = await productApiInstance.get(`/detail/${productId}`)
     return response.data
 }
+
+export async function addProductVariant(productId, newProductVariant) {
+
+   const formData = new FormData()
+
+   newProductVariant.images.forEach((image) => {
+   formData.append(`images`, image.file)
+   })
+
+   formData.append("stock", newProductVariant.stock)
+   formData.append("attributes", JSON.stringify(newProductVariant.attributes))
+   formData.append("price", newProductVariant.price)
+
+   const response = await productApiInstance.post(`/${productId}/variants`, formData)
+   return response.data
+}   
